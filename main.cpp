eee5fd4ffc9bb09054997f10b66cd1929e097729
Unevenmetalic "Kris" Prankster <33995085+GithubPrankster@users.noreply.github.com>
2019-03-15 17:58:50 -0300

    Lightning Update
    yep

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <limits>
 #include <chrono>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STBI_MSC_SECURE_CRT
 #include "stb_image_write.h"
 
 const int width = 640, height = 480;
@@ -67,6 +68,13 @@ struct Material{
 	Material() = default;
 };
 
+struct Light{
+	Vec pos;
+	double intensity;
+	Light(Vec p, double i) : pos(p), intensity(i) {}
+	Light() = default;
+};
+
 struct Sphere{
 	Vec pos;
 	double radius;
@@ -74,39 +82,66 @@ struct Sphere{
 	
 	Sphere(Vec c, double r, Material mat) : pos(c), radius(r), material(mat) {}
 	
-	bool intersect(Vec orig, Vec dir, double t0){
+	bool intersect(Vec orig, Vec dir, double &t2){
+		double radius2 = radius * radius;
 		Vec L = pos - orig;
+
 		double tca = L.dot(dir);
 		double d2 = L.square() - tca * tca;
-		if (d2 > radius * radius) return false;
-		double thc = sqrtf(radius * radius - d2);
-		t0 = tca - thc;
+
+		if (d2 > radius2) return false;
+
+		double thc = sqrtf(radius2 - d2);
+
+		double t0 = tca - thc;
 		double t1 = tca + thc;
-		if (t0 < 0) t0 = t1;
-		if (t0 < 0) return false;
+
+		if (t0 > t1) std::swap(t0, t1);
+		if (t0 < 0) {
+			t0 = t1;
+			if(t0 < 0) return false;
+		}
+		t2 = t0;
 		return true;
 	}
 };
 
-bool sceneIntersection(Vec orig, Vec dir, std::vector<Sphere> spheres, Material &objMat){
+bool sceneIntersection(Vec orig, Vec dir, std::vector<Sphere> spheres, Material &objMat, Vec &hitPoint, Vec &normal){
 	double sphere_dist = std::numeric_limits<double>::max();
 	for(size_t i = 0; i < spheres.size(); i++){
-		double dist_i;
+		double dist_i = 0.0;
 		if(spheres[i].intersect(orig, dir, dist_i) && dist_i < sphere_dist){
 			sphere_dist = dist_i;
+			hitPoint = orig + dir * dist_i;
+			normal = (hitPoint - spheres[i].pos).normalize();
 			objMat = spheres[i].material;
 		}
 	}
     return sphere_dist<1000;
 }
 
-Vec cast_ray(Vec orig, Vec dir, std::vector<Sphere> spheres) {
+Vec cast_ray(Vec orig, Vec dir, std::vector<Sphere> spheres, std::vector<Light> lights) {
+	Vec hitPoint = Vec(), normal = Vec();
 	Material obtMat;
 	
-    if (!sceneIntersection(orig, dir, spheres, obtMat)) {
+    if (!sceneIntersection(orig, dir, spheres, obtMat, hitPoint, normal)) {
         return Vec(0.5, 0.6, 5.0); // BG color!
     }
-    return obtMat.color;
+	double totalDt = 0.0;
+	for(size_t i = 0; i < lights.size(); i++){
+		Vec L = (lights[i].pos - hitPoint).normalize();
+		double lightDist = (lights[i].pos - hitPoint).length();
+		
+		Vec shadow_orig = L.dot(normal) < 0 ? hitPoint - normal * 1e-3 : hitPoint + normal * 1e-3;
+        Vec shadow_pt, shadow_N;
+        Material tmpmaterial;
+        if (sceneIntersection(shadow_orig, L, spheres, tmpmaterial, shadow_pt, shadow_N) && (shadow_pt-shadow_orig).length() < lightDist)
+            continue;
+		
+		totalDt += lights[i].intensity * std::max(0.0, L.dot(normal));
+	}
+	
+	return obtMat.color * totalDt;
 }
 
 RGB convertVec(Vec d){
@@ -125,6 +160,9 @@ int main() {
    spheres.push_back(Sphere(Vec(1, 1.2, -17), 2, bluey));
    spheres.push_back(Sphere(Vec(-2, 2, -5), 1.2, greeny));
    spheres.push_back(Sphere(Vec(3, -3, -10), 2.1, greeny));
+   
+   std::vector<Light> lights;
+   lights.push_back(Light(Vec(0, 0, -5), 1.4));
 
    double fov = 3.141596 / 4.0;
    auto timeThen = std::chrono::system_clock::now(), timeNow = std::chrono::system_clock::now();
@@ -141,7 +179,7 @@ int main() {
 		   double x =  (2*(i + 0.5)/(double)width  - 1)*tan(fov/2.)*width/(double)height;
            double y = -(2*(j + 0.5)/(double)height - 1)*tan(fov/2.);
            Vec dir = Vec(x, y, -1).normalize();
-		   data[currentPos] = convertVec(cast_ray(Vec(), dir, spheres));
+		   data[currentPos] = convertVec(cast_ray(Vec(), dir, spheres, lights));
 		   
 		   elapsedTime += deltaTime;
 	   }
